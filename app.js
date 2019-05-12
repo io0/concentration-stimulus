5c893046bde5351e90ea9698cd35da6ef440e02d
Marley <marleyxiong0@gmail.com>
2019-05-12 15:38:16 -0400

    working

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app_express = express();
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
@@ -6,6 +7,9 @@ const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 Express
 */
 // Sets static directory as public
+app_express.use( bodyParser.json() );       // to support JSON-encoded bodies
+app_express.use(express.json());       // to support JSON-encoded bodies
+
 app_express.use(express.static(__dirname + '/public'));
 
 app_express.get('/', (req, res) => {
@@ -16,20 +20,56 @@ app_express.listen(3000, () => {
   console.log('Example app listening on port 3000!')
 });
 
+var date = new Date();
+var day = date.getFullYear() + '-' + (date.getMonth()+1) + '-' +
+                   date.getDate() + '-' + date.getHours() + '-' +
+                   date.getMinutes() + '-' + date.getSeconds();
+
 const csvWriter = createCsvWriter({
-    path: 'data/file.csv',
+    path: 'data/' + day + '.csv',
     header: [
         {id: 'time', title: 'TIME'},
         {id: 'state', title: 'STATE'}
     ]
 });
+const records = [];
+
+function getTimeValue() {
+  var date = new Date();
+  return date.getTime();
+}
+
+function writeCSV() { 
+	csvWriter.writeRecords(records)       // returns a promise
+	    .then(() => {
+	        console.log('...Done');
+	    });
+}
+
+app_express.post('/clicked', (req,res) => {
+	const click = {clickTime: new Date()};
+	console.log(req.body);
+	res.sendStatus(201);
+
+	var interval = req.body.interval;
+	var trialLength = req.body.trialLength;
+
+	records.push({time:getTimeValue(), state:'math'})
+	var i = 0;
+
+	timer = setInterval(function(){
+		if (i * interval < trialLength){
+			records.push({time: getTimeValue(), state:req.body.state});
+		} else {
+			records.push({time: getTimeValue(), state:'end'});
+			writeCSV();
+			clearInterval(timer);
+		}
+		i++;
+	}, interval * 1000);
+	console.log(records)
+
+
+	console.log(req.body.interval);
+});
  
-const records = [
-    {time: 123,  state: 'active'},
-    {time: 'Mary', state: 'English'}
-];
- 
-csvWriter.writeRecords(records)       // returns a promise
-    .then(() => {
-        console.log('...Done');
-    });
--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -8,7 +8,12 @@ max = 30;
 function getRandom() {
   min = Math.ceil(min);
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+  rand = Math.floor(Math.random() * (max - min + 1)) + min;
+  while (rand % 10 == 0){ // exclude numbers that are multiples of 10
+  	rand = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+  return rand;
+   //The maximum is inclusive and the minimum is inclusive 
 }
 
 function updateText(){
@@ -16,16 +21,67 @@ function updateText(){
 	$("#text").text(str);
 }
 
-$("#start").click(function() {
+$("#startMath").click(function() {
 	console.log("click");
 	var interval = $("#input").val();
-	var max_times = $("#trialLength").val();
+	var trialLength = $("#trialLength").val();
+	const arr = {"interval": interval, "trialLength": trialLength, "state": "math"};
+
+	fetch('/clicked', 
+		{
+			method: 'POST', 
+			body: JSON.stringify(arr),
+			headers: { "Content-Type": "application/json" }
+		}
+	)
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    });
 	var i = 0;
 	updateText();
 	timer = setInterval(function(){
-		if (i * interval < max_times){
+		if (i * interval < trialLength){
 			updateText();
+			var date = new Date();
+			console.log(date.getTime());
 		} else {
+			$("#text").text(".");
+			clearInterval(timer);
+		}
+		i++;
+	}, interval * 1000);
+});
+
+$("#startRest").click(function() {
+	console.log("click");
+	var interval = $("#input").val();
+	var trialLength = $("#trialLength").val();
+	const arr = {"interval": interval, "trialLength": trialLength, "state": 'rest'};
+
+	fetch('/clicked', 
+		{
+			method: 'POST', 
+			body: JSON.stringify(arr),
+			headers: { "Content-Type": "application/json" }
+		}
+	)
+    .then(function(response) {
+      if(response.ok) {
+        console.log('Click was recorded');
+        return;
+      }
+      throw new Error('Request failed.');
+    });
+	var i = 0;
+	$("#text").text("-");
+	timer = setInterval(function(){
+		if (i * interval < trialLength){
+		} else {
+			$("#text").text(".");
 			clearInterval(timer);
 		}
 		i++;
